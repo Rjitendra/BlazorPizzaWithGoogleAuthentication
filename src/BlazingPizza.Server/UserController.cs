f35dd492e050f73e2bec0c847c60143746edec93
jitendra <jbehera@bridgetree.com>
2020-02-12 08:11:20 +0530

    Project SetUp

---
+++ src/BlazingPizza.Server/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Twitter;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Server
+{
+    [ApiController]
+    public class UserController : Controller
+    {
+        private static UserInfo LoggedOutUser = new UserInfo { IsAuthenticated = false };
+
+        [HttpGet("user")]
+        public UserInfo GetUser()
+        {
+            return User.Identity.IsAuthenticated
+                ? new UserInfo { Name = User.Identity.Name, IsAuthenticated = true }
+                : LoggedOutUser;
+        }
+
+        [HttpGet("user/signin")]
+        public async Task SignIn(string redirectUri)
+        {
+            if (string.IsNullOrEmpty(redirectUri) || !Url.IsLocalUrl(redirectUri))
+            {
+                redirectUri = "/";
+            }
+
+            await HttpContext.ChallengeAsync(
+               GoogleDefaults.AuthenticationScheme,
+                new AuthenticationProperties { RedirectUri = redirectUri });
+        }
+
+        [HttpGet("user/signout")]
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("~/");
+        }
+    }
+}
